63db2fa52be9fc47061ee6b8f42d970c9a7263b4
nikoro1904 <niko.rosskopf@t-online.de>
2021-06-24 09:35:35 +0200

    added question to tutorial 4

---
+++ videos/pwm_tutorial/tutorial4_question.ino
@@ -0,0 +1,20 @@
+int led = 9;
+int Helligkeit = 0;
+int aenderung = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(led, Helligkeit);
+  Helligkeit = Helligkeit + aenderung;
+  if (Helligkeit >= 255 || Helligkeit <= 0) { 
+    // Beachte, dass ">" mit ">=" und "<" mit "<" ausgetauscht wurde, das war auch ein Fehler im Code
+    //Helligkeit = -Helligkeit; // Diese Zeile war FALSCH!
+    aenderung = -aenderung;     // Diese Zeile ist dafür richtig.
+  }
+  delay(50); // das Delay von 50 Millisekunden verzögert alles etwas, sodass die LED langsamer dimmt
+}
